68c081b58033db73b162c03aa15ceab90ab92b7f
ziginsider <ziginsider@gmail.com>
2017-11-28 13:29:14 +0300

    toolbar main activity

--- app/src/main/java/io/github/ziginsider/firebasedemo/MainActivity.java
+++ app/src/main/java/io/github/ziginsider/firebasedemo/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.ziginsider.firebasedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Firebase demo");
         setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
