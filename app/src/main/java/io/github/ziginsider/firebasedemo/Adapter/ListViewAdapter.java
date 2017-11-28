cc086097c1f987c3b5a522dcec72db0462c24ec0
ziginsider <ziginsider@gmail.com>
2017-11-28 15:19:29 +0300

    Control add

---
+++ app/src/main/java/io/github/ziginsider/firebasedemo/Adapter/ListViewAdapter.java
@@ -0,0 +1,57 @@
+package io.github.ziginsider.firebasedemo.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import io.github.ziginsider.firebasedemo.Model.User;
+import io.github.ziginsider.firebasedemo.R;
+
+/**
+ * Created by zigin on 28.11.2017.
+ */
+
+public class ListViewAdapter extends BaseAdapter {
+
+    Activity activity;
+    List<User> listUsers;
+    LayoutInflater inflater;
+
+    @Override
+    public int getCount() {
+        return listUsers.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return listUsers.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        inflater = (LayoutInflater) activity
+                .getBaseContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View itemView = inflater.inflate(R.layout.listview_item, null);
+
+        TextView txtUser = (TextView) itemView.findViewById(R.id.txt_name);
+        TextView txtEmail = (TextView) itemView.findViewById(R.id.txt_email);
+
+        txtUser.setText(listUsers.get(i).getName());
+        txtEmail.setText(listUsers.get(i).getEmail());
+
+        return  itemView;
+    }
+}
--- app/src/main/java/io/github/ziginsider/firebasedemo/MainActivity.java
+++ app/src/main/java/io/github/ziginsider/firebasedemo/MainActivity.java
@@ -4,9 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText input_name, input_email;
+    private ListView list_data;
+    private ProgressBar circular_progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Firebase demo");
         setSupportActionBar(toolbar);
+
+        //Control
+        circular_progress = (ProgressBar) findViewById(R.id.circular_progress);
+        input_name = (EditText) findViewById(R.id.name);
+        input_email = (EditText) findViewById(R.id.email);
+        list_data = (ListView) findViewById(R.id.list_data);
+
+
+
+
 
     }
 
