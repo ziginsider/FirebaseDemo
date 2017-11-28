af2722c4004e74b1d78b2ef2b69d6ba30121f05b
ziginsider <ziginsider@gmail.com>
2017-11-28 17:23:15 +0300

    begin Firebase Api

--- app/src/main/java/io/github/ziginsider/firebasedemo/MainActivity.java
+++ app/src/main/java/io/github/ziginsider/firebasedemo/MainActivity.java
@@ -4,9 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     EditText input_name, input_email;
     private ListView list_data;
     private ProgressBar circular_progress;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +42,35 @@ public class MainActivity extends AppCompatActivity {
         input_email = (EditText) findViewById(R.id.email);
         list_data = (ListView) findViewById(R.id.list_data);
 
+        //Firebase
+        initFirebase();
+        addEventFirebaseListener();
 
 
+    }
 
+    private void addEventFirebaseListener() {
+        circular_progress.setVisibility(View.VISIBLE);
+        list_data.setVisibility(View.INVISIBLE);
+
+        mDatabaseReference.child("user")
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initFirebase() {
+        FirebaseApp.initializeApp(this);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference();
 
     }
 
