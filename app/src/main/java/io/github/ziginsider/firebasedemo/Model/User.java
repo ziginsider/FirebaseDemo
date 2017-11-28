2fe1e642495d770be7f4b9f5d98d75e048a82a0b
ziginsider <ziginsider@gmail.com>
2017-11-28 14:21:26 +0300

    user add

--- app/src/main/java/io/github/ziginsider/firebasedemo/Model/User.java
+++ app/src/main/java/io/github/ziginsider/firebasedemo/Model/User.java
@@ -5,4 +5,38 @@ package io.github.ziginsider.firebasedemo.Model;
  */
 
 public class User {
+    String uid, name, email;
+
+    public User() {
+    }
+
+    public User(String uid, String name, String email) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
